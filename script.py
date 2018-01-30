85f65d2805f1b87bcd2d5e8afc933b9ee18a0cf0
annelhote <annelhote@gmail.com>
2018-01-30 15:08:17 +0100

    Collect personnal data

--- script.py
+++ script.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 # Execution example : python script.py
+# Documentation : https://developer.vimeo.com/api/start
 
 
 #
 # Libs
 #
+import json
 import logging
+import os
+import vimeo
 
 
 #
@@ -14,13 +18,33 @@ import logging
 #
 log_file = 'script.log'
 log_level = logging.DEBUG
+conf_file = 'conf.json'
 
 
 #
 # Functions
 #
 def main() :
-	print "MAIN"
+	# Load conf file
+	if os.path.exists(conf_file) :
+		with open(conf_file) as f :
+			conf = json.load(f)
+		logging.info('Conf file loaded')
+	else :
+		logging.error('No conf file provided or wrong file name : ' + conf_file)
+		sys.exit(0)
+	# Connect to Vimeo account
+	v = vimeo.VimeoClient(
+		token = conf['token'],
+		key = conf['key'],
+		secret = conf['secret']
+	)
+	# Make the request to the server for the "/me" endpoint.
+	about_me = v.get('/me')
+	# Make sure we got back a successful response.
+	assert about_me.status_code == 200
+	# Load the body's JSON data.
+	print about_me.json()
 
 #
 # Main
